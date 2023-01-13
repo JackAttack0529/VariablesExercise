14b0bfc0ad28cf3d6deed661b293aa9fbe3a0221
JackAttack0529 <121962728+JackAttack0529@users.noreply.github.com>
2023-01-13 09:46:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// const 
+let num = 20;
+
+ num = 15; 
+console.log(num);
+ num = num + 7 
+console.log(num);
+ num --;
+ console.log(num);
+  num = "This Is Not A Number"
+  console.log(num);
+let blank;
+console.log(blank);
+blank = "null"
+console.log(blank);
+
+//BONUS
+const quarter = 0.25; dime = 0.10; nickel = 0.05; penny = 0.01;
+console.log(quarter + " " + dime + " " + nickel + " " + penny)
+
+let letters = "qwertyuiop"
+console.log(letters[letters.length - 1]);
